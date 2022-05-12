cd7a6a1c7a611412c2dd1e8e5e5f021c49b3e5ab
CdeCASA <105052321+CdeCASA@users.noreply.github.com>
2022-05-12 09:03:23 +0200

    Update instalacion.sh

--- instalacion.sh
+++ instalacion.sh
@@ -34,7 +34,7 @@ echo "+                                                                     +"
 echo "+                                                                     +"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 sleep 2
-sudo apt install apache2
+sudo apt install apache2 -y
 sleep 2
 clear
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
@@ -47,38 +47,96 @@ echo "+                                                                     +"
 echo "+                                                                     +"
 echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 sleep 2
-sudo apt install php
+sudo apt install php -y
+sleep 2
+clear
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+                 Instalación de GIT                                  +"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+sleep 2
+sudo apt install git -y
+sleep 2
+clear
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+                 Descargamos de Github                               +"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+cd /var/www/html
+sudo git clone https://github.com/CdeCASA/Projecte.git
+cd /home/$USER
+clear
 
-echo "Usuario"
+#############
+
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+                 Administración del Apache                           +"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+                                                                     +"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo
+echo
+echo
+echo "A continuacion vamos a crear un usuario, este usuario será el que podra acceder al panel de control"
+echo
+echo
+echo
+echo "Dame el nombre del usuario a crear"
 read usuario
 sudo htpasswd -c /etc/apache2/.segura $usuario
-sudo rm /etc/apache2/sites-available/000-default.conf
-echo '<VirtualHost *:80>' >> /etc/apache2/sites-available/000-default.conf
-echo 'ServerAdmin webmaster@localhost' >> /etc/apache2/sites-available/000-default.conf
-echo 'DocumentRoot /var/www/html' >> /etc/apache2/sites-available/000-default.conf
-echo 'ErrorLog ${APACHE_LOG_DIR}/error.log' >> /etc/apache2/sites-available/000-default.conf
-echo 'CustomLog ${APACHE_LOG_DIR}/access.log combined' >> /etc/apache2/sites-available/000-default.conf
-echo '<Directory "/var/www/html/">' >> /etc/apache2/sites-available/000-default.conf
-echo 'AuthType Basic' >> /etc/apache2/sites-available/000-default.conf
-echo 'AuthName "Privat"' >> /etc/apache2/sites-available/000-default.conf
-echo 'AuthBasicProvider file' >> /etc/apache2/sites-available/000-default.conf
-echo 'AuthUserFile "/etc/apache2/.segura"' >> /etc/apache2/sites-available/000-default.conf
-echo 'Require valid-user' >> /etc/apache2/sites-available/000-default.conf
-echo '</Directory>' >> /etc/apache2/sites-available/000-default.conf
-echo '</VirtualHost>' >> /etc/apache2/sites-available/000-default.conf
 
+###############
+
+sudo rm /var/www/html/index.html
+sudo chmod -R 777 /var/www/html/Projecte
+sudo echo '127.0.0.1    panelcontrol.com' >> /etc/hosts
+################
+
+sudo echo '<VirtualHost *:*>' >> /etc/apache2/sites-available/projecte.conf
+sudo echo 'ServerName panelcontrol.com' >> /etc/apache2/sites-available/projecte.conf
+sudo echo 'ServerAdmin webmaster@localhost' >> /etc/apache2/sites-available/projecte.conf
+sudo echo 'DocumentRoot /var/www/html/Projecte/' >> /etc/apache2/sites-available/projecte.conf
+sudo echo 'ErrorLog ${APACHE_LOG_DIR}/error.log' >> /etc/apache2/sites-available/projecte.conf
+sudo echo 'CustomLog ${APACHE_LOG_DIR}/access.log combined' >> /etc/apache2/sites-available/projecte.conf
+sudo echo '<Directory "/var/www/html/Projecte/">' >> /etc/apache2/sites-available/projecte.conf
+sudo echo 'AuthType Basic' >> /etc/apache2/sites-available/projecte.conf
+sudo echo 'AuthName "Projecte"' >> /etc/apache2/sites-available/projecte.conf
+sudo echo 'AuthBasicProvider file' >> /etc/apache2/sites-available/projecte.conf
+sudo echo 'AuthUserFile "/etc/apache2/.segura"' >> /etc/apache2/sites-available/projecte.conf
+sudo echo 'Require valid-user' >> /etc/apache2/sites-available/projecte.conf
+sudo echo '</Directory>' >> /etc/apache2/sites-available/projecte.conf
+sudo echo '</VirtualHost>' >> /etc/apache2/sites-available/projecte.conf
+a2ensite projecte.conf
+a2dissite 000-default.conf
 sudo systemctl restart apache2.service
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+clear
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "+                                                                                                           +"
+echo "+                                                                                                           +"
+echo "+                                                                                                           +"
+echo "+                 Instalacion terminada                                                                     +"
+echo "+                                                                                                           +"
+echo "+                                                                                                           +"
+echo "+                 Para acceder al panel de control tiene dos formas de acceder:                             +"
+echo "+                                                                                                           +"
+echo "+                 Mediante -> localhost                                                                     +"
+echo "+                             panelcontrol.com                                                              +"
+echo "+                                                                                                           +"
+echo "+                 El usuario para acceder es el creado con anterioridad                                     +"
+echo "+                                                                                                           +"
+echo "+                                                                                                           +"
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
